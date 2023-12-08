edb1637b50ddf982af877a906b9bf46781886ca1
Recovery Eyo <eyorecovery95@gmail.com>
2023-12-08 02:13:00 +0100

    added a state manager

--- ecommerce/src/Header.js
+++ ecommerce/src/Header.js
@@ -1,51 +1,59 @@
-import React from 'react'
-import './header.css'
-import { Link } from 'react-router-dom'
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import React from "react";
+import "./header.css";
+import { Link } from "react-router-dom";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  // Example of correct usage in Header.js:
+  const [{ basket }] = useStateValue();
+  console.log(basket);
+
   return (
-    <nav className='header'>
-     <Link to='/'> <img className='header-logo' src='./images/ecommerce.png'alt=''/></Link>
-     <div className='header-search'>
-     <input type='text' className='header-searchInput'/>
-     <SearchOutlinedIcon className='hearder-searchIcon'/>
-     </div>
+    <nav className="header">
+      <Link to="/">
+        {" "}
+        <img className="header-logo" src="./images/ecommerce.png" alt="" />
+      </Link>
+      <div className="header-search">
+        <input type="text" className="header-searchInput" />
+        <SearchOutlinedIcon className="hearder-searchIcon" />
+      </div>
 
-     <div className='header-nav'>
+      <div className="header-nav">
+        <Link to="/login" className="header-link">
+          <div className="header-option">
+            <span className="header-optionLineOne">Hello, Recovery</span>
+            <span className="header-optionLineTwo">Sign in </span>
+          </div>
+        </Link>
 
-    <Link to='/login' className='header-link'>
-    <div className='header-option'>
-    <span  className='header-optionLineOne'>Hello, Recovery</span>
-    <span  className='header-optionLineTwo'>Sign in </span>
-    </div>
-    </Link>
+        <Link to="/" className="header-link">
+          <div className="header-option">
+            <span className="header-optionLineOne">Returns</span>
+            <span className="header-optionLineTwo">& orders</span>
+          </div>
+        </Link>
 
-    <Link to='/' className='header-link'>
-    <div className='header-option'>
-    <span className='header-optionLineOne'>Returns</span>
-    <span  className='header-optionLineTwo'>& orders</span>
-    </div>
-    </Link>
+        <Link className="header-link">
+          <div className="header-option">
+            <span className="header-optionLineOne">Your</span>
+            <span className="header-optionLineTwo">Prime</span>
+          </div>
+        </Link>
 
-    <Link className='header-link'>
-    <div className='header-option'>
-    <span  className='header-optionLineOne'>Your</span>
-    <span  className='header-optionLineTwo'>Prime</span>
-    </div>
-    </Link>
-
-    <Link to='/checkout' className='header-link'>
-    <div className='header-optionBasket'>
-      <ShoppingBasketIcon/>
-      <span className='header-optionLineTwo header-basketCount' >0</span>
-    </div>
-    </Link>
-
-     </div>
+        <Link to="/checkout" className="header-link">
+          <div className="header-optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header-optionLineTwo header-basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- ecommerce/src/reducer.js
+++ ecommerce/src/reducer.js
@@ -1,18 +1,21 @@
 export const initialState = {
   basket: [],
 };
-
 //the first file sets up the data layer so that it could work
 //reducer is used to listen for action and to update the state of the store
 
 function reducer(state, action) {
+  console.log(action);
   switch (action.type) {
     case "ADD_TO_BASKET":
       // logic of adding item to basket
-      break;
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
     case "REMOVE_FROM_BASKET":
       //logic of removing from basket
-      break;
+      return { state };
     default:
       return state;
   }
